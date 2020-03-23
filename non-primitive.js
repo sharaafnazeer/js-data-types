2a85f27647bf3eeca10863370c5932c1b914fb83
sharaafnazeer <ahamedsharaaf@gmail.com>
2020-03-23 23:44:55 +0530

    initial

---
+++ non-primitive.js
@@ -0,0 +1,14 @@
+
+
+const object = {
+'firstName' : 'Tony',
+'lastName' : 'Stark'	
+}
+
+console.log(object);
+
+
+object.isSuperHero = true;
+object.isAvenger = true;
+
+console.log(object);
---
+++ primitive.js
@@ -0,0 +1,10 @@
+
+const number = 42;
+
+console.log(number);
+
+number = 50;
+
+console.log(50);
+
+
